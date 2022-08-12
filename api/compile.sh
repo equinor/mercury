52c895e5b0c425967966153e6ba7eb0ee50868f8
Marius Lie Winger <mlw@equinor.com>
2022-08-12 16:10:57 +0200

    build: Change dockerfile to use intels fortran compiler

--- api/compile.sh
+++ api/compile.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-set -eux
 
-export LDFLAGS="-static"
+export LDFLAGS="-static -static-intel"
 
 rm *.mod
 
-gfortran -std=legacy -c thermclc@proc.f90
-gfortran -std=legacy -c UNIFACfast.f90 thermclc.f90
+ifort -c thermclc@proc.f90
+ifort -c UNIFACfast.f90 thermclc.f90
 
 echo "Compiling object files..."
 echo ""
-gfortran -fPIC -static -c multiflash.f90 Stability.f90 MIXRULESfast.f90 UNIFACfast.f90 chemeq.f90 thermclc.f90
+ifort -fPIC -static -c -static-intel multiflash.f90 Stability.f90 MIXRULESfast.f90 UNIFACfast.f90 chemeq.f90 thermclc.f90
 
 ar rc my_lib.a thermclc.o multiflash.o Stability.o MIXRULESfast.o UNIFACfast.o chemeq.o
 
 echo "Compiling python usable module..."
 echo ""
-unset LDFLAGS
-f2py --fcompiler=gfortran -c program.f90 -m libhg my_lib.a
+f2py --fcompiler=intelem -c program.f90 -m libhg my_lib.a
 
-mv libhg.cpython-310-x86_64-linux-gnu.so ../libhg.so
+cp libhg.cpython-310-x86_64-linux-gnu.so libhg.so
+cp libhg.so ../libhg.so
 
+rm libhg.cpython-310-x86_64-linux-gnu.so
