87d8e35b7a9009369503e8c2f7a92568adceb77a
Marius Lie Winger <89073985+mariuswinger@users.noreply.github.com>
2022-09-01 09:51:52 +0200

    feat: add components api endpoint

--- api/src/features/multiflash/multiflash_feature.py
+++ api/src/features/multiflash/multiflash_feature.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
+from starlette.responses import JSONResponse
 
+from common.responses import create_response
 from entities.Multiflash import Multiflash
 from features.multiflash.multiflash_use_case import (
     MultiflashResponse,
@@ -9,6 +11,7 @@ from features.multiflash.multiflash_use_case import (
 router = APIRouter(tags=["multiflash"])
 
 
+@create_response(JSONResponse)
 @router.post("/multiflash", operation_id="compute_multiflash", response_model=MultiflashResponse)
 async def compute_multiflash(multiflash: Multiflash) -> MultiflashResponse:
     return compute_multiflash_use_case(multiflash=multiflash)
