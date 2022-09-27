a082d2040cbc98781181950aef4656f5d0fb4958
Marius Lie Winger <89073985+mariuswinger@users.noreply.github.com>
2022-09-27 15:52:26 +0200

    feat: add component response model

--- api/src/features/component/component_feature.py
+++ api/src/features/component/component_feature.py
@@ -1,16 +1,14 @@
-from typing import Dict
-
 from fastapi import APIRouter
 from starlette.responses import JSONResponse
 
 from common.components import COMPONENT_IDS
 from common.responses import create_response
-from features.component.component_use_case import component_use_case
+from entities.ComponentResponse import ComponentResponse
 
 router = APIRouter(tags=["component"])
 
 
 @create_response(JSONResponse)
-@router.get("/components", operation_id="get_components", response_model=Dict[str, Dict[str, str]])
-async def components() -> Dict[str, Dict[str, str]]:
-    return component_use_case(COMPONENT_IDS)
+@router.get("/components", operation_id="get_components", response_model=ComponentResponse)
+async def components() -> ComponentResponse:
+    return ComponentResponse(components=COMPONENT_IDS)
--- api/src/tests/integration/features/component/test_component_feature.py
+++ api/src/tests/integration/features/component/test_component_feature.py
@@ -1,14 +1,15 @@
 from starlette.status import HTTP_200_OK
 
 from common.components import COMPONENT_IDS
-from features.component.component_use_case import ComponentName
+from entities.ComponentResponse import ComponentName
 
 
 def test_components_feature(test_app):
     response = test_app.get("/api/v1/components")
     results = response.json()
+    components = results["components"]
     # using sets as sets do not care about ordering
-    assert {key for key in results.keys()} == set(COMPONENT_IDS.keys())
-    for key, value in results.items():
+    assert {key for key in components.keys()} == set(COMPONENT_IDS.keys())
+    for key, value in components.items():
         assert value == ComponentName(**COMPONENT_IDS[key]).dict(by_alias=True)
     assert response.status_code == HTTP_200_OK
