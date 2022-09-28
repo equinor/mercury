57247cd6c9f4f112c71b52ba2eb269d0372c93e6
Marius Lie Winger <89073985+mariuswinger@users.noreply.github.com>
2022-09-28 09:52:05 +0200

    review: add suggestions

--- api/src/tests/integration/features/component/test_component_feature.py
+++ api/src/tests/integration/features/component/test_component_feature.py
@@ -9,7 +9,7 @@ def test_components_feature(test_app):
     results = response.json()
     components = results["components"]
     # using sets as sets do not care about ordering
-    assert {key for key in components.keys()} == set(COMPONENT_IDS.keys())
+    assert set(components.keys()) == set(COMPONENT_IDS.keys())
     for key, value in components.items():
         assert value == ComponentName(**COMPONENT_IDS[key]).dict(by_alias=True)
     assert response.status_code == HTTP_200_OK
