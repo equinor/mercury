2eecbbcc71cf4c76fc87cddbfba8d83aa97b3aa8
Marius Lie Winger <89073985+mariuswinger@users.noreply.github.com>
2022-10-27 00:32:22 -0700

    feat: add sum for component ratios (#150)
    * feat: add sum for component ratios
    
    * fix: pr comments

---
+++ web/src/components/feature/package_dialog/ComponentTableSum.tsx
@@ -0,0 +1,29 @@
+import { EdsProvider, Table } from '@equinor/eds-core-react'
+import { TComponentRatios } from '../../../types'
+import { formatNumber } from '../../../tableUtils'
+
+function computeComponentRatioSum(componentRatios: TComponentRatios) {
+  return formatNumber(
+    Object.values(componentRatios).reduce((a, b) => a + b, 0),
+    3
+  )
+}
+
+export const ComponentTableSum = ({
+  componentRatios,
+}: {
+  componentRatios: TComponentRatios
+}): JSX.Element => {
+  return (
+    <EdsProvider density={'compact'}>
+      <Table>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>{`Component sum`}</Table.Cell>
+            <Table.Cell>{computeComponentRatioSum(componentRatios)}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </EdsProvider>
+  )
+}
