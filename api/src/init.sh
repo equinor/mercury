38f5603e0da351a013c04adf2b9ffc6f803d2cc4
Marius Lie Winger <89073985+mariuswinger@users.noreply.github.com>
2022-11-04 01:08:08 -0700

    feat: add version number to header (#176)
    * feat: add version number to header
    
    * fix: add empty version.txt
    
    * ci: version.txt for api

--- api/src/init.sh
+++ api/src/init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
 
+cat version.txt
+
 if [ "$1" = 'api' ]; then
   if [ "${ENVIRONMENT:-'local'}" != "local" ]; then
     gunicorn app:create_app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
