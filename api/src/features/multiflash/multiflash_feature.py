16375dc15ad2f5f330b2b8609ea7bb56320429ab
Stig Ofstad <stigofstad@gmail.com>
2022-11-07 13:01:26 +0100

    feature(api): expose metrics

--- api/src/features/multiflash/multiflash_feature.py
+++ api/src/features/multiflash/multiflash_feature.py
@@ -3,6 +3,7 @@ from starlette.responses import JSONResponse
 
 from common.responses import create_response
 from entities.Multiflash import Multiflash
+from features.metrics.metrics_feature import metrics
 from features.multiflash.multiflash_use_case import (
     MultiflashResponse,
     compute_multiflash_use_case,
@@ -14,4 +15,5 @@ router = APIRouter(tags=["multiflash"])
 @create_response(JSONResponse)
 @router.post("/multiflash", operation_id="compute_multiflash", response_model=MultiflashResponse)
 async def compute_multiflash(multiflash: Multiflash) -> MultiflashResponse:
+    metrics.MULTIFLASH_CALCULATION_COUNT += 1
     return compute_multiflash_use_case(multiflash=multiflash)
