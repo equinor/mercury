39b37d7636384eec7bfb2d0a9aecee8bb77b034c
Marius Lie Winger <89073985+mariuswinger@users.noreply.github.com>
2022-11-21 08:42:29 +0100

    fix: increase timeout for long computations

--- api/src/init.sh
+++ api/src/init.sh
@@ -5,7 +5,7 @@ cat version.txt
 
 if [ "$1" = 'api' ]; then
   if [ "${ENVIRONMENT:-'local'}" != "local" ]; then
-    gunicorn app:create_app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
+    gunicorn app:create_app --timeout 60 --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
   else
     python3 /code/app.py run
   fi
