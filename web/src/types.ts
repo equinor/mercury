821f25c68af12610c5165c7490103d76342f1266
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2022-12-06 12:28:06 +0100

    feature: Add versioning to local storage
    Refs #288

--- web/src/types.ts
+++ web/src/types.ts
@@ -3,15 +3,23 @@ import { ComponentProperties, MultiflashResponse } from './api/generated'
 export type TComponentProperty = ComponentProperties & {
   id: string
 }
+
 export type TResults = MultiflashResponse & {
   cubicFeedFlow: number
 }
+
 export type TComponentRatio = {
   id: string
   ratio: string
 }
+
 export type TFeedUnit = 'kg/d' | 'Sm3/d'
+
 export type TFeedFlow = { unit: TFeedUnit; value: number }
+
+/**
+ * Note: Fluids are stored in local storage using type TPackage[]. Updating this type definition may therefore cause the application to break in unexpected ways. To avoid that, remember to increment the version number given to useLocalStorage.
+ */
 export type TPackage = {
   name: string
   description: string
