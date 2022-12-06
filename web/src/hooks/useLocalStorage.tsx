5a9706cf0ce9b77ff4d37d2471e0c9d4c59713bd
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2022-12-06 12:28:06 +0100

    feature: Add versioning to local storage
    Refs #288

--- web/src/hooks/useLocalStorage.tsx
+++ web/src/hooks/useLocalStorage.tsx
@@ -1,15 +1,36 @@
 import { useState } from 'react'
 
-function useLocalStorage<T>(key: string, initialValue: T): [T, (v: T) => void] {
+function useLocalStorage<T>(
+  key: string,
+  initialValue: T,
+  version: number
+): [T, (v: T) => void] {
   const [storedValue, setStoredValue] = useState<T>(() => {
-    if (typeof window === 'undefined') {
-      return initialValue
-    }
+    if (typeof window === 'undefined') return initialValue
+
     const item = window.localStorage.getItem(key)
-    try {
-      return item ? JSON.parse(item) : initialValue
-    } catch (error) {
-      return initialValue
+    if (item === null) return initialValue
+
+    const parsed = (() => {
+      try {
+        return JSON.parse(item)
+      } catch {
+        return null
+      }
+    })()
+    if (parsed === null) return initialValue
+
+    if (
+      typeof parsed === 'object' &&
+      'version' in parsed &&
+      'data' in parsed &&
+      parsed.version === version
+    ) {
+      return parsed.data
+    } else {
+      throw TypeError(
+        `The type of stored value "${key}" is outdated. Please reset the application data`
+      )
     }
   })
 
@@ -19,7 +40,10 @@ function useLocalStorage<T>(key: string, initialValue: T): [T, (v: T) => void] {
         value instanceof Function ? value(storedValue) : value
       setStoredValue(valueToStore)
       if (typeof window !== 'undefined') {
-        window.localStorage.setItem(key, JSON.stringify(valueToStore))
+        window.localStorage.setItem(
+          key,
+          JSON.stringify({ version: version, data: valueToStore })
+        )
       }
     } catch (error) {
       console.log(`Failed to store value '${value}' for key '${key}'`)
