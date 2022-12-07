2f9c25eec44b457aa1954d0409201366c9bec749
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2022-12-07 09:04:07 +0100

    test: Make tests pass again
    Refs #260

--- web/src/feature/results/PhaseTable.test.tsx
+++ web/src/feature/results/PhaseTable.test.tsx
@@ -6,9 +6,9 @@ import { mockResults } from '../../setupTests'
 test('renders without crashing and displaying correct values in table', async () => {
   render(<PhaseTable results={mockResults} />)
   // @ts-ignore because not able to get eslint to discover these types
-  expect(screen.getByTestId('Vapor-1')).toHaveTextContent('9577 μg/Sm3')
+  expect(screen.getByTestId('0-Vapor-0')).toHaveTextContent('9577 μg/Sm3')
   // @ts-ignore because not able to get eslint to discover these types
-  expect(screen.getByTestId('Mercury-2')).toHaveTextContent('1')
+  expect(screen.getByTestId('0-Mercury-1')).toHaveTextContent('1')
   // @ts-ignore because not able to get eslint to discover these types
-  expect(screen.getByTestId('Aqueous-3')).toHaveTextContent('0.001504 g/d')
+  expect(screen.getByTestId('0-Aqueous-2')).toHaveTextContent('0.001504 g/d')
 })
