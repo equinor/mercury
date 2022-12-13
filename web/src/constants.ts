a10b113a564a037639b20de69ed7b0ee636e48cf
Stig Ofstad <stigofstad@gmail.com>
2022-12-13 12:37:22 +0100

    fix: optional refresh when auth session expires

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,12 +1,18 @@
+import { TRefreshTokenExpiredEvent } from 'react-oauth2-code-pkce'
 export const AUTH_DISABLED = process.env.REACT_APP_AUTH !== '1'
 
 export const authConfig = {
-  clientId: process.env.REACT_APP_AUTH_CLIENT_ID || '',
-  authorizationEndpoint: process.env.REACT_APP_AUTH_ENDPOINT || '',
-  tokenEndpoint: process.env.REACT_APP_TOKEN_ENDPOINT || '',
-  scope: process.env.REACT_APP_AUTH_SCOPE || '',
+  clientId: '0cd10735-a40c-4ff6-84c8-74e3b54ed93f',
+  authorizationEndpoint:
+    'https://login.microsoftonline.com/3aa4a235-b6e2-48d5-9195-7fcf05b459b0/oauth2/v2.0/authorize',
+  tokenEndpoint:
+    'https://login.microsoftonline.com/3aa4a235-b6e2-48d5-9195-7fcf05b459b0/oauth2/v2.0/token',
+  scope: 'api://0cd10735-a40c-4ff6-84c8-74e3b54ed93f/All',
   redirectUri: process.env.REACT_APP_AUTH_REDIRECT_URI || '',
-  logoutEndpoint: process.env.REACT_APP_LOGOUT_ENDPOINT || '',
+  onRefreshTokenExpire: (event: TRefreshTokenExpiredEvent) =>
+    window.confirm(
+      'Session expired. Refresh page to continue using the site?'
+    ) && event.login(),
 }
 
 // Molecular weight of mercury
