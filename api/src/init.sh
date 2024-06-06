c461b8547880497c82129f6f6b6f7bb9ca3e0084
Stig Ofstad <stigofstad@gmail.com>
2024-06-06 12:16:34 +0200

    chore: bump dependencies

--- api/src/init.sh
+++ api/src/init.sh
@@ -4,11 +4,7 @@ set -eu
 cat version.txt
 
 if [ "$1" = 'api' ]; then
-  if [ "${ENVIRONMENT:-'local'}" != "local" ]; then
-    gunicorn app:create_app --timeout 60 --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
-  else
-    python3 /code/app.py run
-  fi
+  python3 /code/app.py run
 else
   exec "$@"
 fi
