c522478495e146f3e95f442324119d5f66ac6b8e
Christopher Collin Løkken <43639886+collinlokken@users.noreply.github.com>
2025-02-24 13:49:05 +0100

    ci: test compiling uisng gfortran

--- api/compile.sh
+++ api/compile.sh
@@ -5,18 +5,21 @@ export LDFLAGS="-static-intel"
 
 rm *.mod
 
-ifort -c thermclc@proc.f90
-ifort -c UNIFACfast.f90 thermclc.f90
+ifx -c thermclc@proc.f90
+ifx -c UNIFACfast.f90 thermclc.f90
 
 echo "Compiling object files..."
 echo ""
-ifort -fPIC -static -c -static-intel multiflash.f90 Stability.f90 MIXRULESfast.f90 UNIFACfast.f90 thermclc.f90
+ifx -fPIC -static -c -static-intel multiflash.f90 Stability.f90 MIXRULESfast.f90 UNIFACfast.f90 thermclc.f90
 
 ar rc my_lib.a thermclc.o multiflash.o Stability.o MIXRULESfast.o UNIFACfast.o
 
 echo "Compiling python usable module..."
 echo ""
-f2py --fcompiler=intelem -c mercury.f90 -m libhg my_lib.a
+# Set the F2PY environment variable to use ifx
+# f2py --fcompiler=g95 -c mercury.f90 -m libhg my_lib.a
+
+python3 -c "import fmodpy; fmodpy.fimport('mercury.f90', dependencies=['libhg', 'my_lib.a'])"
 
 cp libhg.cpython-310-x86_64-linux-gnu.so libhg.so
 cp libhg.so ../libhg.so
