14766666ceedf92f0434ca3a2dd70d5df940c78d
Marius Lie Winger <89073985+mariuswinger@users.noreply.github.com>
2025-02-26 15:24:02 +0100

    ci: substitute deprecated ifort compiler with ifx

--- api/compile.sh
+++ api/compile.sh
@@ -5,18 +5,18 @@ export LDFLAGS="-static-intel"
 
 rm *.mod
 
-ifort -c thermclc@proc.f90
-ifort -c UNIFACfast.f90 thermclc.f90
+ifx -c thermclc@proc.f90
+ifx -c UNIFACfast.f90 thermclc.f90
 
 echo "Compiling object files..."
 echo ""
-ifort -fPIC -static -c -static-intel multiflash.f90 Stability.f90 MIXRULESfast.f90 UNIFACfast.f90 thermclc.f90
+ifx -fPIC -static -c -static-intel multiflash.f90 Stability.f90 MIXRULESfast.f90 UNIFACfast.f90 thermclc.f90
 
 ar rc my_lib.a thermclc.o multiflash.o Stability.o MIXRULESfast.o UNIFACfast.o
 
 echo "Compiling python usable module..."
 echo ""
-f2py --fcompiler=intelem -c mercury.f90 -m libhg my_lib.a
+F90=ifx f2py --fcompiler=intelem -c mercury.f90 -m libhg my_lib.a
 
 cp libhg.cpython-310-x86_64-linux-gnu.so libhg.so
 cp libhg.so ../libhg.so
