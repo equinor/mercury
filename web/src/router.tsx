790db845effa18ebc136fdc6915561f39551f70f
Marius Lie Winger <89073985+mariuswinger@users.noreply.github.com>
2026-01-27 14:29:28 +0100

    _feat: add InvalidUrl page

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom'
+import InvalidUrl from './pages/InvalidUrl/InvalidUrl'
 import { MainPage } from './pages/MainPage/MainPage'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <MainPage />,
+  },
+  {
+    path: '*',
+    element: <InvalidUrl />,
   },
 ])
