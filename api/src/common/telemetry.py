fb15dbc6bfe8ff191445c0a0e4d12f018c22c965
Marius Lie Winger <89073985+mariuswinger@users.noreply.github.com>
2026-02-04 08:25:34 +0100

    refactor: adjust local telemetry setting

--- api/src/common/telemetry.py
+++ api/src/common/telemetry.py
@@ -2,12 +2,14 @@ from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
+from common.logger_level import LoggerLevel
 from config import config
 
 # Creates a tracer from the global tracer provider
 # If APPINSIGHTS_CONSTRING is not set, the tracer will log to console
-if not config.APPLICATIONINSIGHTS_CONNECTION_STRING:
+if not config.APPLICATIONINSIGHTS_CONNECTION_STRING and config.LOGGER_LEVEL == LoggerLevel.DEBUG:
     trace_provider = TracerProvider()
     trace_provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
     trace.set_tracer_provider(trace_provider)
+
 tracer = trace.get_tracer("tracer.global")
